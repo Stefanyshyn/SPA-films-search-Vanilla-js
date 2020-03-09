977c37b1fea69d4bcf750daf9f1921b815526265
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-03-09 22:49:40 +0200

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options:{
+            babelrc: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }    
+    ]
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'public', to: '.' },
+    ])
+  ]
+};
