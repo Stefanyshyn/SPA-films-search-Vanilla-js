102586fc5d230f2ed4576c4cee3ee158dd87a2e6
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-03-10 20:23:09 +0200

    feat: get movie by name

--- src/events/SearchForm.js
+++ src/events/SearchForm.js
@@ -1,21 +1,22 @@
 
 console.log(process.env.API_MOIVE_DATABASE)
 
-const searchFilms = (e)=>{
+const searchFilms = async (e)=>{
     let search = document.getElementById("search-field");
     if(!search)return;
-    console.log(1)
-    let result = fetch(`https://api.themoviedb.org/3/search/movie`,
-        {
-            "api_key":process.env.API_MOIVE_DATABASE,
-            "language":'en-US',
-            "page":1,
-            "include_adult":false
-        }
-    )
-    console.log(result.json())
-    console.log(2)
     
+    let nameMoive = search.value+"";
+    if(!nameMoive.trim()) return;
+
+    let result = await fetch('https://api.themoviedb.org/3/search/movie?'
+    + `&` + `api_key=${process.env.API_MOIVE_DATABASE}`
+    + `&` + `language=${`en-US`}`
+    + `&` + `query=${nameMoive}`
+    + `&` + `include_adult=${false}`
+    )
+    let json = await result.json();
+    let data = json.link.data;
+    return data;
 }
 
 export default searchFilms;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import searchFilms from "./events/SearchForm"
 import search from "./components/SearchForm";
 document.getElementById('button-search-form').addEventListener("click", searchFilms)
-
-
