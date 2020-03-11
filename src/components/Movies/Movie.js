eb6869da5fef10da75e4bea7aac837fa05627a80
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-03-11 09:13:27 +0200

    style: added style to movie component

--- src/components/Movies/Movie.js
+++ src/components/Movies/Movie.js
@@ -3,11 +3,11 @@ import toHTML from '../../untils/convert'
 const createMovie = (movie)=>{
     let Movie = `
     <div class="movie">
-        <img class="movie-img" src="${process.env.IMAGE_PATH + movie.poster_path}" alt="Image">
+        <div class="movie-img">
+            <img src="${process.env.IMAGE_PATH + movie.poster_path}" alt="Image">
+        </div>
         <div class="movie-description">
-            <h4>Title:</h4>
-            <p>${movie.title}</p>
-            <h4>Overview:</h4>
+            <h4>${movie.title}</h4>
             <p>${movie.overview}</p>
         </div>
     </div>
