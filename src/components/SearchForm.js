d1b4111575250d044ac4dd5cf64c267dbf0617b1
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2020-03-13 19:52:29 +0200

    feat: added logo and event click

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,16 +1,25 @@
 import toHTML from '../untils/convert';
 import clickOnBtnSearchMovie from "../controllers/searchFormController/clickOnBtnSearchMovie"
+import loadPopularMovies from "../controllers/movieController/loadPopularMovies"
 
 const SearchForm = ()=>{
     let form = `
         <div  class='searchForm'>
+            <img src="../../public/main.png" alt="Image">
             <input id="search-field" class="form-control" type="text"/>
             <button id="button-search-form" class="btn btn-dark">Saerch</button>
         </div>
     `
     const formElement = toHTML(form);
 
-    formElement.children[1].addEventListener("click", clickOnBtnSearchMovie)
+    formElement.children[0].addEventListener("click", loadPopularMovies)
+    formElement.children[1].addEventListener('keypress', function (e) {
+        console.log(e.key) 
+        if (e.key === 'Enter') {
+            clickOnBtnSearchMovie();
+        }
+    })
+    formElement.children[2].addEventListener("click", clickOnBtnSearchMovie)
 
     return formElement;
 }
